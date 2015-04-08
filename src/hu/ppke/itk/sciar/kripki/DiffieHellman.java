47c4340d709d5a194e279301d9529204368858e8
Ármin Scipiades <scipiades.armin@gmail.com>
2015-04-08 04:39:23 +0200

    Read DH primes

--- src/hu/ppke/itk/sciar/kripki/DiffieHellman.java
+++ src/hu/ppke/itk/sciar/kripki/DiffieHellman.java
@@ -2,25 +2,63 @@ package hu.ppke.itk.sciar.kripki;
 
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Scanner;
-import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.*;
 import java.math.BigInteger;
+
+import org.apache.commons.lang3.StringUtils;
 
 
 public class DiffieHellman {
-	static Map<Integer, BigInteger> moduli = new HashMap<Integer, BigInteger>();
-	static {
+	private static Map<Integer, BigInteger> loadModuli(String fname) {
+		Map<Integer, BigInteger> Result = new HashMap<Integer, BigInteger>();
 		try {
-			Scanner scanner = new Scanner(new File("res/DHmods.txt"));
-			int r = scanner.nextInt(10);
-			scanner.next("\\.\\*");
+			List<String> lines = Files.readAllLines(Paths.get(fname), StandardCharsets.UTF_8);
+
+			Pattern declaration = Pattern.compile("(\\d+).*");
+			boolean declare = true;
+			Integer bits = null;
+			List<String> valparts = new ArrayList<String>();
+			for(String line : lines) {
+				if(declare && !StringUtils.isBlank(line)) {
+					Matcher match = declaration.matcher(line);
+					if(!match.matches()) throw new RuntimeException(String.format("In %s expected modulus declaration, got '%s'", fname, line));
+					bits = Integer.valueOf(match.group(1));
+					declare = false;
+				} else if(!declare) {
+					if(StringUtils.isBlank(line)) {	// finish up
+						String val = StringUtils.join(valparts, "");
+						if(StringUtils.isBlank(val)) throw new RuntimeException(String.format("Empty value for bit %d in %s", bits, fname));
+						if(val.length()*4 != bits) throw new RuntimeException(String.format("Was promised %d bits, got %s bits in %s", bits, val.length()*4, fname));
+
+						Result.put(bits, new BigInteger(val, 16));
+
+						valparts = new ArrayList<String>();
+						bits = null;
+						declare = true;
+					} else { // gobble more value parts
+						valparts.addAll( Arrays.asList(StringUtils.split(line, " ")) );
+					}
+				}
+			}
 		} catch(Exception e) {
-			throw new RuntimeException("Couldn't read the DH primes!", e);
+			throw new RuntimeException(e);
 		}
 
+		return Result;
 	}
 
+	public static void main(String[] args) throws Exception  {
+		Map<Integer, BigInteger> moduli = loadModuli("res/DHmods.txt");
+		for(Map.Entry<Integer, BigInteger> entry : moduli.entrySet()) {
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
+		}
+	}
 
-
-
+	private static final Map<Integer, BigInteger> moduli = DiffieHellman.loadModuli("res/DHMods.txt");
 }
